a5cd657418e9376f33b9154a2583db058dada1bf
nick <nick.thenebels@gmail.com>
2020-02-27 11:21:45 -0800

    thirdpartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,12 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'YlIE7PMYTP9dexHdi2uSOwQLo5JOT8Xl';
+	var map = L.mapquest.map('map', {
+		center: [32.878643, -117.235586],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	L.marker([32.878643, -117.235586]).addTo(map);
 }
